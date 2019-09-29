cacbad57828ca6cfebb2ac4094d3a0adb07372bb
github username <github email address>
2019-09-28 17:53:04 -0700

    updated url consts

--- pokemon-frontend/src/index.js
+++ pokemon-frontend/src/index.js
@@ -1,6 +1,6 @@
-const baseUrl = "http://localhost:3000"
-const pokemons = baseUrl+"/pokemons"
-const trainers = baseUrl+"/trainers"
+const BASE_URL = "http://localhost:3000"
+const TRAINERS_URL = `${BASE_URL}/trainers`
+const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 document.addEventListener("DOMContentLoaded", main)
 
